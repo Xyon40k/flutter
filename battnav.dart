c1ab464838f53d88f91fc9f74dcee8e27d54f64c
Xyon40k <110849666+Xyon40k@users.noreply.github.com>
2023-04-04 20:08:33 +0200

    Add files via upload

--- battnav.dart
+++ battnav.dart
@@ -33,10 +33,12 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   final int size = 10;
-  final int difficulty = 4;
+  final int difficulty = 3;
   late int bombs;
   late int flags;
+  late int toclick;
   late Timer timer;
+  late Color bgcolor;
   int elapsed = 0;
   bool stopped = false;
   bool settingup = false;
@@ -44,6 +46,7 @@ class _MyHomePageState extends State<MyHomePage> {
   late List<Widget> _containers;
   late List<List<bool>> _bombs;
   late List<List<bool>> _clicked;
+  late List<List<bool>> _flags;
 
   final List<double> difficulties = [8, 6.5, 5, 4];
   Map<int, Color> colors = {
@@ -71,11 +74,18 @@ class _MyHomePageState extends State<MyHomePage> {
     _containers = [];
     _bombs = [];
     _clicked = [];
+    _flags = [];
 
     bombs = (size * size / difficulties[difficulty - 1]).floor();
     flags = bombs;
+    toclick = size * size - bombs;
     elapsed = 0;
     stopped = false;
+    bgcolor = Colors.white;
+
+    createfields();
+
+    placebombs();
 
     for (int i = 1; i < size + 1; i++) {
       for (int j = 1; j < size + 1; j++) {
@@ -83,21 +93,18 @@ class _MyHomePageState extends State<MyHomePage> {
           Container(
             color: const Color.fromARGB(255, 196, 196, 196),
             alignment: Alignment.center,
-            padding: const EdgeInsets.all(1),
-            child: IconButton(
-              icon: const Icon(null),
-              onPressed: () {
-                reveal(i, j);
-              },
-            ),
+            child: TextButton(
+                onPressed: () {
+                  reveal(i, j);
+                },
+                onLongPress: () {
+                  flag(i, j);
+                },
+                child: const Text("")),
           ),
         );
       }
     }
-
-    createfields();
-
-    placebombs();
 
     timer = Timer.periodic(const Duration(seconds: 1), (timer) {
       setState(() {
@@ -112,9 +119,11 @@ class _MyHomePageState extends State<MyHomePage> {
     for (int i = 0; i < size + 2; i++) {
       _bombs.add(<bool>[]);
       _clicked.add(<bool>[]);
+      _flags.add(<bool>[]);
       for (int j = 0; j < size + 2; j++) {
         _bombs[i].add(false);
         _clicked[i].add(false);
+        _flags[i].add(false);
       }
     }
   }
@@ -130,6 +139,7 @@ class _MyHomePageState extends State<MyHomePage> {
         bombs--;
       }
     }
+    bombs = flags;
   }
 
   // TODO: doesnt work
@@ -154,6 +164,10 @@ class _MyHomePageState extends State<MyHomePage> {
       return;
     }
 
+    if (_flags[y][x]) {
+      return;
+    }
+
     if (_clicked[y][x]) {
       return;
     }
@@ -174,6 +188,7 @@ class _MyHomePageState extends State<MyHomePage> {
       return;
     }
 
+    toclick--;
     int num = near(y, x);
 
     if (num == 0) {
@@ -197,33 +212,88 @@ class _MyHomePageState extends State<MyHomePage> {
               Text("$num", style: TextStyle(fontSize: 26, color: colors[num])));
     }
 
+    if (toclick == 0) {
+      win();
+    }
+
     setState(() {
       _containers[(y - 1) * size + (x - 1)] = toadd;
     });
+  }
+
+  void flag(int y, int x) {
+    if (stopped) {
+      return;
+    }
+
+    bool newstate = !_flags[y][x];
+    setState(() {
+      _flags[y][x] = newstate;
+      newstate ? flags-- : flags++;
+      _containers[(y - 1) * size + (x - 1)] = newstate
+          ? Container(
+              color: const Color.fromARGB(255, 196, 196, 196),
+              alignment: Alignment.center,
+              child: Stack(alignment: Alignment.center, children: [
+                const Icon(Icons.flag, size: 30, color: Colors.red),
+                TextButton(
+                    onPressed: () {
+                      reveal(y, x);
+                    },
+                    onLongPress: () {
+                      flag(y, x);
+                    },
+                    child: const Text("")),
+              ]),
+            )
+          : Container(
+              color: const Color.fromARGB(255, 196, 196, 196),
+              alignment: Alignment.center,
+              child: TextButton(
+                onPressed: () {
+                  reveal(y, x);
+                },
+                onLongPress: () {
+                  flag(y, x);
+                },
+                child: const Text(""),
+              ),
+            );
+    });
+    print("Flagged: $y $x");
   }
 
   void lose() {
     timer.cancel();
     stopped = true;
 
+    revealbombs();
+  }
+
+  void win() {
+    timer.cancel();
+    stopped = true;
+
+    setState(() {
+      bgcolor = Colors.green;
+    });
+  }
+
+  void revealbombs() {
     for (int i = 1; i < size + 1; i++) {
       for (int j = 1; j < size + 1; j++) {
-        if (_bombs[i][j]) {
-          setState(() {
-            _containers[(i - 1) * size + (j - 1)] = Container(
-                color: Colors.white,
-                alignment: Alignment.center,
-                padding: const EdgeInsets.all(1),
-                child: const Icon(Icons.sunny, color: Colors.black));
-          });
+        if (_bombs[i][j] && !_flags[i][j]) {
+          _containers[(i - 1) * size + (j - 1)] = Container(
+              color: Colors.white,
+              alignment: Alignment.center,
+              padding: const EdgeInsets.all(1),
+              child: const Icon(Icons.sunny, color: Colors.black));
         }
       }
     }
   }
 
-  List<Widget> getcontainers() {
-    return _containers;
-  }
+  List<Widget> getcontainers() => _containers;
 
   @override
   Widget build(BuildContext context) {
@@ -231,47 +301,50 @@ class _MyHomePageState extends State<MyHomePage> {
       appBar: AppBar(
         title: Text(widget.title),
       ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget>[
-          Flexible(
-              flex: 0,
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Row(children: [
-                    const SizedBox(width: 30),
-                    const Icon(Icons.flag, color: Colors.red, size: 32),
-                    Text(" $flags ", style: const TextStyle(fontSize: 22)),
-                  ]),
-                  Row(children: [
-                    Text("Elapsed: $elapsed",
-                        style: const TextStyle(fontSize: 22)),
-                    const SizedBox(width: 30),
-                  ]),
-                ],
-              )),
-          const Flexible(flex: 1, child: Icon(null)),
-          Flexible(
-            flex: 8,
-            child:
-                Column(mainAxisAlignment: MainAxisAlignment.start, children: [
-              Container(
-                  color: Colors.white,
-                  padding: const EdgeInsets.all(5),
-                  child: Container(
-                      color: Colors.black,
-                      padding: const EdgeInsets.all(5),
-                      child: GridView.count(
-                          crossAxisCount: size,
-                          shrinkWrap: true,
-                          mainAxisSpacing: 2,
-                          crossAxisSpacing: 2,
-                          children: List.from(_containers))))
-            ]),
-          )
-        ],
-      ),
+      body: Container(
+          color: bgcolor,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Flexible(
+                  flex: 0,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Row(children: [
+                        const SizedBox(width: 30),
+                        const Icon(Icons.flag, color: Colors.red, size: 32),
+                        Text(" $flags ", style: const TextStyle(fontSize: 22)),
+                      ]),
+                      Row(children: [
+                        Text("Elapsed: $elapsed",
+                            style: const TextStyle(fontSize: 22)),
+                        const SizedBox(width: 30),
+                      ]),
+                    ],
+                  )),
+              const Flexible(flex: 1, child: Icon(null)),
+              Flexible(
+                flex: 8,
+                child: Column(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      Container(
+                          color: bgcolor,
+                          padding: const EdgeInsets.all(5),
+                          child: Container(
+                              color: Colors.black,
+                              padding: const EdgeInsets.all(5),
+                              child: GridView.count(
+                                  crossAxisCount: size,
+                                  shrinkWrap: true,
+                                  mainAxisSpacing: 2,
+                                  crossAxisSpacing: 2,
+                                  children: List.from(_containers))))
+                    ]),
+              )
+            ],
+          )),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
           if (!settingup) {
